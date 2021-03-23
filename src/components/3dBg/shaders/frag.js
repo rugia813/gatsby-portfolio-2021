f700b4a2e9444321e0846f65829d84845cf8125e
Janway <Janway.Li@goldnet.com>
2021-03-23 17:31:49 +0800

    don't flick too much

--- src/components/3dBg/shaders/frag.js
+++ src/components/3dBg/shaders/frag.js
@@ -10,7 +10,7 @@ varying vec4 vPressure;
 void main() {
 	vec4 diffuseColor = texture2D( map, vUv );
 	float z = smoothstep(0.4, 1.0, vPos.z);
-	float d = z + cos(time*5.) * .1;
+	float d = z + cos(time*3.) * 0.1;
 	gl_FragColor = diffuseColor * vec4(d * 1.15, d * 1.85, 1.6, 1.0) + vPressure;
 
 	// if ( diffuseColor.w < 0.5 ) discard;
