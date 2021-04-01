11ff39ca1bb3e79a608343cb6ded04086ba82109
Janway <Janway.Li@goldnet.com>
2021-04-01 14:41:40 +0800

    WIP cover image transition

--- src/pages/project-list.js
+++ src/pages/project-list.js
@@ -1,8 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import styled from "@emotion/styled"
 import { graphql } from 'gatsby'
 import { mobile, desktop } from "../styles/consts";
 import { BaseButton } from "../components/baseButton";
+
+const moveCoverFrom = `
+    position: fixed;
+    top: var(--top);
+    left: var(--left);
+`
+const moveCoverTo = `
+    position: fixed;
+    top: 5px;
+    left: 30%;
+`
 
 const ProjectPanel = styled.div`
     display: grid;
@@ -29,6 +40,11 @@ const ProjectPanel = styled.div`
         align-items: center;
         justify-items: start;
         grid-template-rows: 1fr 12px 2fr 1fr;
+        transition: filter 1.5s;
+
+        &.hidden {
+            filter: opacity(0);
+        }
 
         ${desktop} {
             max-width: 500px;
@@ -57,21 +73,57 @@ const ProjectPanel = styled.div`
     }
 
     .cover {
+        --top: 0;
+        --left: 0;
+
+        img {
+            width: 100%;
+        }
+
         ${desktop} {
             grid-row: 2;
             justify-self: baseline;
-            img {
-                width: 33vw;
+            width: 33vw;
+            transition: width 1s ease-in-out;
+
+            &.detail {
+                animation: moveCover 1s forwards ease-in-out;
+                width: 43vw;
+            }
+            &.moveBack {
+                animation: moveCover2 1s ease-in-out;
             }
         }
         ${mobile} {
             grid-row: 1;
             place-self: baseline;
             margin: auto;
-            img {
-                max-width: 75vw;
-            }
+            max-width: 75vw;
         }
+    }
+    @keyframes moveCover {
+        from {
+            ${moveCoverFrom}
+        }
+        to {
+            ${moveCoverTo}
+        }
+    }
+    @keyframes moveCover2 {
+        from {
+            ${moveCoverTo}
+        }
+        to {
+            ${moveCoverFrom}
+        }
+    }
+`
+
+const DetailPanel = styled.div`
+
+
+    img {
+        max-width: 33vw;
     }
 `
 
@@ -87,6 +139,24 @@ export default function ProjectList({ data }) {
         tags: node.frontmatter.tags,
     }))
     const total = nodes.length
+
+    const coverRef = useRef(null)
+    function openDetail(e) {
+        const cover = coverRef.current
+        if (cover.classList.contains('detail')) {
+            cover.classList.remove('detail')
+            cover.classList.add('moveBack')
+            setInDetail(false)
+        } else {
+            cover.style.setProperty('--top', cover.offsetTop + 'px')
+            cover.style.setProperty('--left', cover.offsetLeft + 'px')
+            cover.classList.add('detail')
+            cover.classList.remove('moveBack')
+            setInDetail(true)
+        }
+    }
+
+    const [inDetail, setInDetail] = useState(false)
 
     const [locked, setLocked] = useState(false)
     function changeCur(e) {
@@ -117,18 +187,22 @@ export default function ProjectList({ data }) {
 
     return  (
         <ProjectPanel onWheel={changeCur}>
-            <div className="project-info">
+            <div className={'project-info ' + (inDetail && 'hidden')}>
                 <h1>{cur.title}</h1>
                 <div className="pageNum">{idx + 1} / {total}</div>
-                <BaseButton className="detailBtn">Detail</BaseButton>
+                <BaseButton className="detailBtn" onClick={openDetail}>Detail</BaseButton>
                 <div className="pageNav">
                     <span onClick={() => goUp(idx)}>Previous</span>
                     <span onClick={() => goDown(idx)}>Next</span>
                 </div>
             </div>
-            <div className="cover">
+            <div className="cover" ref={coverRef}>
                 <img src={cur.cover} alt={cur.title + ' image'} />
             </div>
+            {inDetail && <DetailPanel>
+                <h1>{cur.title}</h1>
+                <div dangerouslySetInnerHTML={{__html: cur.content + ''}} />
+            </DetailPanel>}
         </ProjectPanel>
     )
 }
